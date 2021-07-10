b7ce91a60e908625ce62aa250fdf2796a48e21aa
reem-alqurm <ralqurm@gmail.com>
2021-07-11 00:25:51 +0300

    slider is finshed

--- src/components/Main-page/Main.js
+++ src/components/Main-page/Main.js
@@ -2,24 +2,25 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Carousel } from 'react-bootstrap';
 import { Typography } from '@material-ui/core/';
+import './Main.scss'
 
-const image1 = 'https://images.pexels.com/photos/4145190/pexels-photo-4145190.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
-const image2 = 'https://images.pexels.com/photos/4145153/pexels-photo-4145153.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
-const image3 = 'https://images.pexels.com/photos/3059748/pexels-photo-3059748.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+const image1 = 'https://lighthouse-tc.com/wp-content/uploads/2020/08/e-learning-header-bg.jpg';
+const image2 = 'https://production-tcf.imgix.net/app/uploads/2020/03/20155636/dudley_opm-01.png?auto=format%2Ccompress&q=80&fit=crop&w=1200&h=600';
+const image3 = 'https://static.voices.com/wp-content/uploads/2019/03/MR-3017-industry-elearning-1.jpg';
 
 const useStyles = makeStyles({
     carousel: {},
     image_one: {
         width: 1370,
-        height: 500,
+        height: 520,
     },
     image_two: {
         width: 1370,
-        height: 500,
+        height: 520,
     },
     image_third: {
         width: 1370,
-        height: 500,
+        height: 520,
     },
     text: {
         marginTop: 40,
@@ -42,12 +43,27 @@ export default function Main(props){
                 <Carousel fade className={classes.carousel}>
                     <Carousel.Item interval={2000}>
                         <img className={classes.image_one} src={image1} alt="First slide" />
+                        <div class="carousel-caption d-none d-md-block">
+                           <h2>TechUni</h2>
+                              <h5>
+                                  Your Campas All In Your Hands
+                              </h5>
+                       </div>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
                         <img className={classes.image_two}  src={image2} alt="Second slide" />
+                        <div class="carousel-caption-2 d-none d-md-block">
+                           <h2>A great place to educat,</h2>
+                           <h5>Learn and Interact ! </h5>
+                       </div>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
                         <img className={classes.image_third} src={image3} alt="Third slide" />
+                        <div class="carousel-caption-3 d-none d-md-block">
+                           <h2> Learning Without Limits</h2>
+                              <h5>
+                              A classroom for all with the ability to chat and video call with your colleagues.                              </h5>
+                       </div>
                     </Carousel.Item>
                 </Carousel>
             </div>
