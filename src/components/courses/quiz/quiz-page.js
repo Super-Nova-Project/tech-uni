3b756ae8e910c9d222824fc5800bd895aaa93abe
Malak Al-Momani <malak.momani.96@gmail.com>
2021-07-12 01:31:22 +0300

    end quiz page

--- src/components/courses/quiz/quiz-page.js
+++ src/components/courses/quiz/quiz-page.js
@@ -4,9 +4,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Paper, Typography, Grid, Container } from '@material-ui/core/';
 import { useParams } from "react-router";
 import cookie from 'react-cookies';
-import Question from './question';
 import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Radio from '@material-ui/core/Radio'
 import Show from '../../Show'
+// import {NavigateNext , ArrowBack} from '@material-ui/icons'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import Timer from './timer';
+import Slide from '@material-ui/core/Slide';
+import { auto } from '@popperjs/core';
 
 const API_SERVER = 'https://eraser-401.herokuapp.com';
 
@@ -21,6 +30,17 @@ const useStyles = makeStyles((theme) => ({
   },
   question: {
     padding: 30
+  },
+  formControl: {
+    margin: theme.spacing(3),
+  },
+  button: {
+    margin: theme.spacing(1, 1, 0, 0),
+  },
+  result:{
+    marginTop:'5%',
+    textAlign:'center',
+    backgroundColor:'lightgrey'
   }
 }));
 
@@ -44,14 +64,75 @@ function shuffleArray(array) {
   return array;
 }
 
-export default function CenteredGrid() {
+export default function OneQuiz() {
   const classes = useStyles();
   const [currentQuiz, setCurrentQuiz] = useState({});
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [result, setResult] = useState(false);
+  const [answers, setAnswers] = React.useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = React.useState({ question: '', options: [], correct_answer: '' });
+  // const [timeLeft, setTimeLeft] = useState(0);
+  // const [timer, setTimer] = useState(0);
+  // const [loading, setLoading] = React.useState(true);
+  const [finish, setFinish] = useState(false);
+  const [start, setStart] = useState(false);
   const [grade, setGrade] = useState(0);
   const { id, quizID } = useParams();
 
+
+  function renderQuiz() {
+
+    return (
+      <div>
+        <Show condition={!finish}>
+          <Grid>
+            <Container maxWidth="md">
+              <h3> <strong>
+                {currentQuestion.question}
+              </strong>
+              </h3>
+
+              <form>
+                <FormControl component="fieldset" className={classes.formControl}>
+                  <RadioGroup aria-label="quiz" name="quiz" onChange={handleRadioChange}>
+                    {
+                      currentQuestion.options.map((option, idx) => {
+                        return (
+                          <FormControlLabel key={idx} value={option} control={<Radio />} label={option} />
+                        )
+                      })}
+                  </RadioGroup>
+                </FormControl>
+                <Grid>
+                  <Button disabled={currentQuestionIndex === 0} onClick={handleBack} variant="contained" className={classes.button}>
+                    <ArrowBackIosIcon />
+                  </Button>
+                  <Button
+                    variant="contained"
+                    onClick={currentQuestionIndex === l - 1 ? handleResult : handleNext}
+                    className={classes.button}
+                  >
+                    {currentQuestionIndex === l - 1 ? 'Finish' : <ArrowForwardIosIcon />}
+                  </Button>
+                </Grid>
+              </form>
+            </Container>
+          </Grid >
+        </Show>
+        <Show condition={finish}>
+          <Slide in={finish} direction="left">
+            <Container maxWidth="md" className={classes.result}>
+              <Typography variant="h3" gutterBottom>
+                YOUR GRADE
+              </Typography>
+              <Typography variant="subtitle1">
+                {grade} Out Of {l}
+              </Typography>
+            </Container>
+          </Slide>
+        </Show>
+      </div>
+    )
+  }
   useEffect(() => {
     const token = cookie.load('auth-token');
     fetch(`${API_SERVER}/course/${id}`, {
@@ -67,26 +148,46 @@ export default function CenteredGrid() {
       // console.log({quizID})
       data.quizes.forEach(quiz => {
         if (quiz._id === quizID)
-          setCurrentQuiz(quiz)
+          setCurrentQuiz(quiz);
+        let arr = shuffleArray(quiz.quizQuestions);
+        setCurrentQuestion(arr[currentQuestionIndex])
       });
     });
   }, []);
 
-  let l = currentQuiz.quizQuestions ? currentQuiz.quizQuestions.length - 1 : 0 ;
+  let l = currentQuiz.quizQuestions ? currentQuiz.quizQuestions.length : 0;
+
+  const handleRadioChange = (event) => {
+    let answer = event.target.value;
+    if (currentQuestion.correct_answer === answer) {
+      setGrade(grade + 1);
+    }
+    console.log(currentQuestion.correct_answer, grade, answer)
+    setAnswers([...answers, answer]);
+
+  };
 
   const handleBack = () => {
     // console.log('-1');
-    setCurrentQuestion((prevActiveStep) => prevActiveStep - 1);
+    setCurrentQuestionIndex((prevActiveStep) => prevActiveStep - 1);
+    setCurrentQuestion(currentQuiz.quizQuestions[currentQuestionIndex]);
+
   };
 
   const handleNext = () => {
-    // console.log('+1');
-    setCurrentQuestion((prevActiveStep) => prevActiveStep + 1);
+    setCurrentQuestionIndex((prevActiveStep) => prevActiveStep + 1);
+    setCurrentQuestion(currentQuiz.quizQuestions[currentQuestionIndex]);
   };
 
   const handleResult = () => {
-    setResult(true);
+    console.log('finish')
+    setFinish(true);
   }
+
+  const handleStart = () => {
+    setStart(true);
+  }
+
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
@@ -95,34 +196,26 @@ export default function CenteredGrid() {
             <Typography variant="h2" gutterBottom>
               {currentQuiz.quizTitle}
             </Typography>
-            <Typography variant="subtitle1" gutterBottom>
-              time remaining : {currentQuiz.timer} minutes
-            </Typography>
-          </Paper>
-        </Grid>
-        <Show condition={!result}>
-          <Container maxWidth="md">
-            <Question index={currentQuestion} question={currentQuiz.quizQuestions? currentQuiz.quizQuestions[currentQuestion] : {}} />
-            <Grid>
-              <Button disabled={currentQuestion === 0} onClick={handleBack} className={classes.button}>
-                Back
-              </Button>
-              <Button
+            <Show condition={!start}>
+              <Button onClick={handleStart}
+                className={classes.button}
                 variant="contained"
                 color="primary"
-                onClick={currentQuestion === l ? handleResult : handleNext}
-                className={classes.button}
-              >
-                {currentQuestion === l ? 'Finish' : 'Next'}
+                size="large"> Start Quiz
               </Button>
-            </Grid>
-          </Container>
-        </Show>
-        
+            </Show>
+            <Show condition={start && !finish}>
+              <Typography variant="subtitle1" gutterBottom>
+                time remaining
+                <Timer initialMinute={currentQuiz.timer} initialSeconds={0} />
+              </Typography>
+            </Show>
+          </Paper>
+          <Show condition={start}>
+            {renderQuiz()}
+          </Show>
+        </Grid>
       </Grid>
-      <Show condition={result}>
-          Congrats!
-      </Show>
 
     </div>
   );
