d98183607cc9355dd448d070728573bba0484902
reem-alqurm <ralqurm@gmail.com>
2021-07-12 16:28:44 +0300

    fixing the vision part and the other task

--- src/components/basics/about-us.js
+++ src/components/basics/about-us.js
@@ -1,4 +1,7 @@
 import Slider from "react-slick";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Component } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./about-us.scss";
@@ -6,10 +9,10 @@ import { Avatar } from "@material-ui/core";
 import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
 import Box from "@material-ui/core/Box";
 import GitHubIcon from "@material-ui/icons/GitHub";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
-import { faQuoteRight } from '@fortawesome/free-solid-svg-icons'
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import { faQuoteRight } from "@fortawesome/free-solid-svg-icons";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import FacebookIcon from "@material-ui/icons/Facebook";
 
 const slidesData = [
@@ -49,6 +52,7 @@ const slidesData = [
       "Loyalty is a two-way street.If Im asking for it from you then youre getting it from me.",
   },
 ];
+
 const PreviousBtn = (props) => {
   console.log(props);
   const { className, onClick } = props;
@@ -68,91 +72,158 @@ const NextBtn = (props) => {
 };
 const Testimonial = () => {
   return (
-    <div
-      className="testimonial"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        marginTop: 50,
-        marginBottom: 70,
-      }}
-    >
-      <div style={{ width: "75%", textAlign: "center" }}>
-        <h1 style={{ marginBottom: 50, color: "#0065E0" }}>Our Team</h1>
-        <Slider prevArrow={<PreviousBtn />} nextArrow={<NextBtn />} dots>
-          <div>
-            <Card img="https://avatars.githubusercontent.com/u/77914628?v=4" />
-            <h3>{slidesData[0].username}</h3>
-            <h6>{slidesData[0].testimonial}</h6>
-            <p><FontAwesomeIcon icon={faQuoteLeft} /> {slidesData[0].qoute} <FontAwesomeIcon icon={faQuoteRight} /></p>
-            <div className="socialIcons">
-              <Box>
-                <a><GitHubIcon /></a>
-                <a><LinkedInIcon /></a>
-                <a><FacebookIcon /></a>
-              </Box>
+    <React.Fragment>
+      <section className="section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 text-center">
+              
+             
+                <div className="card ">
+                  <h3 className="main-heading">Our Vision</h3>
+                      <span className = "visionCap"> Our Vision is 'together for a better future' make the learning easier and anywhere you can take your class and do the assignment that your teacher sign you to do it and everyone grows up with good culture because 'our children the future for the country'.</span>
+                       
+                  <img className="imgvisio"
+                    src="https://www.vippng.com/png/detail/195-1952958_device-allows-3-brains-to-connect-share-thoughts.png"/>
+                </div>
             </div>
           </div>
-          <div>
-            
-            <Card img="https://avatars.githubusercontent.com/u/43066612?v=4" />
-            <h3>{slidesData[2].username}</h3>
-            <h6>{slidesData[2].testimonial}</h6>
-            <p><FontAwesomeIcon icon={faQuoteLeft} /> {slidesData[2].qoute}<FontAwesomeIcon icon={faQuoteRight} /></p>
-            <div className="socialIcons">
-              <Box>
-                <a><GitHubIcon /></a>
-                <a><LinkedInIcon /></a>
-                <a><FacebookIcon /></a>
-              </Box>
-            </div>
-          </div>
-          <div>
-            
-            <Card img="https://avatars.githubusercontent.com/u/77915090?v=4" />
-            <h3>{slidesData[4].username}</h3>
-            <h6>{slidesData[4].testimonial}</h6>
-            <p><FontAwesomeIcon icon={faQuoteLeft} />{slidesData[4].qoute}<FontAwesomeIcon icon={faQuoteRight} /></p>
-            <div className="socialIcons">
-              <Box>
-                <a><GitHubIcon /></a>
-                <a><LinkedInIcon /></a>
-                <a><FacebookIcon /></a>
-              </Box>
-            </div>
-          </div>
-          <div>
-            
-            <Card img="https://avatars.githubusercontent.com/u/78753771?v=4" />
-            <h3>{slidesData[3].username}</h3>
-            <h6>{slidesData[3].testimonial}</h6>
-            <p><FontAwesomeIcon icon={faQuoteLeft} />{slidesData[3].qoute}<FontAwesomeIcon icon={faQuoteRight} /></p>
-            <div className="socialIcons">
-              <Box>
-                <a><GitHubIcon /></a>
-                <a><LinkedInIcon /></a>
-                <a><FacebookIcon /></a>
-              </Box>
-            </div>
-          </div>
-          <div>
-            
-            <Card img="https://avatars.githubusercontent.com/u/77916712?v=4" />
-            <h3>{slidesData[1].username}</h3>
-            <h6>{slidesData[1].testimonial}</h6>
-            <p><FontAwesomeIcon icon={faQuoteLeft} />{slidesData[1].qoute}<FontAwesomeIcon icon={faQuoteRight} /></p>
-            <div className="socialIcons">
-              <Box>
-                <a><GitHubIcon /></a>
-                <a><LinkedInIcon /></a>
-                <a><FacebookIcon /></a>
-              </Box>
-            </div>
+        </div>
+      </section>
 
-          </div>
-        </Slider>
+
+
+      <div
+        className="testimonial"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 0,
+          marginBottom: 70,
+        }}
+      >
+        <div style={{ width: "75%", textAlign: "center" }}>
+          <h3 style={{ marginBottom: 20, color: "#0065E0" }}>Our Team</h3>
+          <Slider prevArrow={<PreviousBtn />} nextArrow={<NextBtn />} dots>
+            <div>
+              <Card img="https://avatars.githubusercontent.com/u/77914628?v=4" />
+              <h3>{slidesData[0].username}</h3>
+              <h6>{slidesData[0].testimonial}</h6>
+              <p>
+                {" "}
+                <FontAwesomeIcon icon={faQuoteLeft} /> {slidesData[0].qoute}{" "}
+                <FontAwesomeIcon icon={faQuoteRight} />{" "}
+              </p>
+              <div className="socialIcons">
+                <Box>
+                  <a>
+                    <GitHubIcon />
+                  </a>
+                  <a>
+                    <LinkedInIcon />
+                  </a>
+                  <a>
+                    <FacebookIcon />
+                  </a>
+                </Box>
+              </div>
+            </div>
+            <div>
+              <Card img="https://avatars.githubusercontent.com/u/43066612?v=4" />
+              <h3>{slidesData[2].username}</h3>
+              <h6>{slidesData[2].testimonial}</h6>
+              <p>
+                {" "}
+                <FontAwesomeIcon icon={faQuoteLeft} /> {slidesData[2].qoute}
+                <FontAwesomeIcon icon={faQuoteRight} />{" "}
+              </p>
+              <div className="socialIcons">
+                <Box>
+                  <a>
+                    <GitHubIcon />
+                  </a>
+                  <a>
+                    <LinkedInIcon />
+                  </a>
+                  <a>
+                    <FacebookIcon />
+                  </a>
+                </Box>
+              </div>
+            </div>
+            <div>
+              <Card img="https://avatars.githubusercontent.com/u/77915090?v=4" />
+              <h3>{slidesData[4].username}</h3>
+              <h6>{slidesData[4].testimonial}</h6>
+              <p>
+                {" "}
+                <FontAwesomeIcon icon={faQuoteLeft} /> {slidesData[4].qoute}{" "}
+                <FontAwesomeIcon icon={faQuoteRight} />{" "}
+              </p>
+              <div className="socialIcons">
+                <Box>
+                  <a>
+                    <GitHubIcon />
+                  </a>
+                  <a>
+                    <LinkedInIcon />
+                  </a>
+                  <a>
+                    <FacebookIcon />
+                  </a>
+                </Box>
+              </div>
+            </div>
+            <div>
+              <Card img="https://avatars.githubusercontent.com/u/78753771?v=4" />
+              <h3>{slidesData[3].username}</h3>
+              <h6>{slidesData[3].testimonial}</h6>
+              <p>
+                {" "}
+                <FontAwesomeIcon icon={faQuoteLeft} /> {slidesData[3].qoute}{" "}
+                <FontAwesomeIcon icon={faQuoteRight} />{" "}
+              </p>
+              <div className="socialIcons">
+                <Box>
+                  <a>
+                    <GitHubIcon />
+                  </a>
+                  <a>
+                    <LinkedInIcon />
+                  </a>
+                  <a>
+                    <FacebookIcon />
+                  </a>
+                </Box>
+              </div>
+            </div>
+            <div>
+              <Card img="https://avatars.githubusercontent.com/u/77916712?v=4" />
+              <h3>{slidesData[1].username}</h3>
+              <h6>{slidesData[1].testimonial}</h6>
+              <p>
+                {" "}
+                <FontAwesomeIcon icon={faQuoteLeft} /> {slidesData[1].qoute}{" "}
+                <FontAwesomeIcon icon={faQuoteRight} />{" "}
+              </p>
+              <div className="socialIcons">
+                <Box>
+                  <a>
+                    <GitHubIcon />
+                  </a>
+                  <a>
+                    <LinkedInIcon />
+                  </a>
+                  <a>
+                    <FacebookIcon />
+                  </a>
+                </Box>
+              </div>
+            </div>
+          </Slider>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
@@ -178,7 +249,6 @@ const Card = ({ img }) => {
           marginBottom: 20,
         }}
       />
-  
     </div>
   );
 };
