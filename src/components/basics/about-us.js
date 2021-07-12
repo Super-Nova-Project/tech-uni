2aa916bf680ffa549564ec0182b4d5c6fbe97292
reem-alqurm <ralqurm@gmail.com>
2021-07-12 13:34:28 +0300

    fished with the about-us

--- src/components/basics/about-us.js
+++ src/components/basics/about-us.js
@@ -4,7 +4,51 @@ import "slick-carousel/slick/slick-theme.css";
 import "./about-us.scss";
 import { Avatar } from "@material-ui/core";
 import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
+import Box from "@material-ui/core/Box";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from "@material-ui/icons/Facebook";
 
+const slidesData = [
+  {
+    id: 1,
+    username: "Ishaq Alathamneh",
+    testimonial: "Software developer",
+    qoute:
+      "You Dont Do The Right Thing For A Reward You Do It Because Its Right",
+  },
+  {
+    id: 2,
+    username: " Musab-Al-omari",
+    testimonial: "Software developer",
+    qoute:
+      'It takes courage to sail into uncharted waters!" "Who said that?" "Snoopy!',
+  },
+  {
+    id: 3,
+    username: "Malak Almomani",
+    testimonial: "Software developer",
+    qoute:
+      " We only live once, Snoopy. Wrong! we only die once.We live every day! ",
+  },
+  {
+    id: 4,
+    username: "Reem Alqurm",
+    testimonial: "Software developer",
+    qoute:
+      " I alone cannot change the world, but I can cast a stone across the water to create many ripples ",
+  },
+  {
+    id: 5,
+    username: "Naeam Mosameh",
+    testimonial: "Software developer",
+    qoute:
+      "Loyalty is a two-way street.If Im asking for it from you then youre getting it from me.",
+  },
+];
 const PreviousBtn = (props) => {
   console.log(props);
   const { className, onClick } = props;
@@ -26,16 +70,86 @@ const Testimonial = () => {
   return (
     <div
       className="testimonial"
-      style={{ display: "flex", justifyContent: "center", marginTop: 50, marginBottom: 50 }}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: 50,
+        marginBottom: 70,
+      }}
     >
-      <div style={{ width: "50%", textAlign: "center" }}>
-        <h1 style={{ marginBottom: 20, color: "#0065E0" }}>Our Team</h1>
+      <div style={{ width: "75%", textAlign: "center" }}>
+        <h1 style={{ marginBottom: 50, color: "#0065E0" }}>Our Team</h1>
         <Slider prevArrow={<PreviousBtn />} nextArrow={<NextBtn />} dots>
-          <Card img="https://avatars.githubusercontent.com/u/77914628?v=4"/>
-          <Card img="https://avatars.githubusercontent.com/u/43066612?v=4" />
-          <Card img="https://avatars.githubusercontent.com/u/77915090?v=4" />
-          <Card img="https://avatars.githubusercontent.com/u/78753771?v=4" />
-          <Card img="https://avatars.githubusercontent.com/u/77916712?v=4" />
+          <div>
+            <Card img="https://avatars.githubusercontent.com/u/77914628?v=4" />
+            <h3>{slidesData[0].username}</h3>
+            <h6>{slidesData[0].testimonial}</h6>
+            <p><FontAwesomeIcon icon={faQuoteLeft} /> {slidesData[0].qoute} <FontAwesomeIcon icon={faQuoteRight} /></p>
+            <div className="socialIcons">
+              <Box>
+                <a><GitHubIcon /></a>
+                <a><LinkedInIcon /></a>
+                <a><FacebookIcon /></a>
+              </Box>
+            </div>
+          </div>
+          <div>
+            
+            <Card img="https://avatars.githubusercontent.com/u/43066612?v=4" />
+            <h3>{slidesData[2].username}</h3>
+            <h6>{slidesData[2].testimonial}</h6>
+            <p><FontAwesomeIcon icon={faQuoteLeft} /> {slidesData[2].qoute}<FontAwesomeIcon icon={faQuoteRight} /></p>
+            <div className="socialIcons">
+              <Box>
+                <a><GitHubIcon /></a>
+                <a><LinkedInIcon /></a>
+                <a><FacebookIcon /></a>
+              </Box>
+            </div>
+          </div>
+          <div>
+            
+            <Card img="https://avatars.githubusercontent.com/u/77915090?v=4" />
+            <h3>{slidesData[4].username}</h3>
+            <h6>{slidesData[4].testimonial}</h6>
+            <p><FontAwesomeIcon icon={faQuoteLeft} />{slidesData[4].qoute}<FontAwesomeIcon icon={faQuoteRight} /></p>
+            <div className="socialIcons">
+              <Box>
+                <a><GitHubIcon /></a>
+                <a><LinkedInIcon /></a>
+                <a><FacebookIcon /></a>
+              </Box>
+            </div>
+          </div>
+          <div>
+            
+            <Card img="https://avatars.githubusercontent.com/u/78753771?v=4" />
+            <h3>{slidesData[3].username}</h3>
+            <h6>{slidesData[3].testimonial}</h6>
+            <p><FontAwesomeIcon icon={faQuoteLeft} />{slidesData[3].qoute}<FontAwesomeIcon icon={faQuoteRight} /></p>
+            <div className="socialIcons">
+              <Box>
+                <a><GitHubIcon /></a>
+                <a><LinkedInIcon /></a>
+                <a><FacebookIcon /></a>
+              </Box>
+            </div>
+          </div>
+          <div>
+            
+            <Card img="https://avatars.githubusercontent.com/u/77916712?v=4" />
+            <h3>{slidesData[1].username}</h3>
+            <h6>{slidesData[1].testimonial}</h6>
+            <p><FontAwesomeIcon icon={faQuoteLeft} />{slidesData[1].qoute}<FontAwesomeIcon icon={faQuoteRight} /></p>
+            <div className="socialIcons">
+              <Box>
+                <a><GitHubIcon /></a>
+                <a><LinkedInIcon /></a>
+                <a><FacebookIcon /></a>
+              </Box>
+            </div>
+
+          </div>
         </Slider>
       </div>
     </div>
@@ -63,18 +177,8 @@ const Card = ({ img }) => {
           padding: 7,
           marginBottom: 20,
         }}
-      />  
-      <p style={{ fontStyle: "italic", marginTop: 25 }}>
-        <span style={{ fontWeight: 500, color: "#0065E0" }}>Ishaq Alathamneh</span> ,
-        Software Developer
-      </p>
-      <p>
-      "You Don't Do The Right Thing For A Reward. You Do It Because It's Right." 
-
-
-
-      </p>
-    
+      />
+  
     </div>
   );
 };
