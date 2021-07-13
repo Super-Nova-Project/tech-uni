6d45ad73ea9b17cf193cf8bfea7fe83dd73b8bc5
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-07-13 18:04:54 +0300

    all done

--- src/components/basics/Logo.js
+++ src/components/basics/Logo.js
@@ -13,7 +13,7 @@ export default function Logo() {
   const classes = useStyles();
   return (
     <>
-      <img src="logo4.png" alt="logo" className={classes.logo} />
+      <img src="./bt.png" alt="logo" className={classes.logo} />
     </>
   )
 };
